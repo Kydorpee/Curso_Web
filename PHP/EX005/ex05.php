2905fa8e455b8f0b930705d66774909c12f2ca02
Kydorpe <lopedro45@gmail.com>
2025-08-28 18:34:00 -0300

    update atividade PHP

---
+++ PHP/EX005/ex05.php
@@ -0,0 +1,13 @@
+
+<?php 
+
+    $valor1 = 20;
+    $valor2 = 30;
+
+    multiplicar($valor1,$valor2);
+    
+    function multiplicar($a,$b)
+    {
+        echo "O seu resultado é: ".($a*$b);
+    }
+?>
